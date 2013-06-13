f872b9ab63bdd83daa2cbe9f07becd47099cd6ba
jeffpowrs <hello@jeffpowersd.com>
2013-06-13 10:46:51 -0400

    If the number given to truncate to is larger than the length of the string do not add ellipsis

--- trunkate.js
+++ trunkate.js
@@ -5,11 +5,12 @@
     var jqArrayLen = jQueryArray.length,
         currEl,
         currText,
-        undo = typeof charNumber === 'string' && charNumber === 'undo' ? true : false ,
+        undo,
         findLastSpace = function () {
+          if( currText.length < charNumber ) return currText.length;
           var i = charNumber+1;
           for ( i >= 0; i--; ) {
-            if ( currText[i] === ' ' ) return charNumber-1;
+            if ( currText[i] === ' ' ) return i+1;
           }
         },
         toOriginal = function () {
@@ -22,10 +23,12 @@
           currEl.text( currEl.text().substr(0, findLastSpace() ) + '...');
         };
 
+    if( typeof charNumber === 'string' && charNumber === 'undo') undo = true;
+
     for ( var i = 0; i < jqArrayLen; i++ ) {
       currEl = $(jQueryArray[i]);
       currText = currEl.data('originalText') || currEl.text();
-      if ( undo ) toOriginal();
+      if ( undo || charNumber > currText.length ) toOriginal();
       else toSubStr();
     }
   
